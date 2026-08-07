8b8c6b99c3f7138007c1a900c386bc25842be901
Ma. Nicole Antonio <antonio.ma.nicole@gmail.com>
2026-08-07 12:03:06 +0800

    Accordion function

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+document.querySelectorAll(".accordion__question").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("active");
+    item.nextElementSibling.classList.toggle("active");
+  });
+});
